c7c38a4ebf92915bdaca8fc46e3f0f0eb2eba45f
VladG <gricenkovlad007@gmail.com>
2025-03-26 21:09:55 +0300

    Add Nav Bar on client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,46 @@
-import MyCard from "./components/Card"
-
-function App() {
-
+import React from 'react';
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import { Menu } from 'antd';
+const items = [
+  {
+    key: 'sub1',
+    label: 'Navigation One',
+    icon: <MailOutlined />,
+    children: [
+      {
+        key: 'g1',
+        label: 'Item 1',
+        type: 'group',
+        children: [
+          { key: '1', label: 'Option 1' },
+          { key: '2', label: 'Option 2' },
+        ],
+      },
+      {
+        key: 'g2',
+        label: 'Item 2',
+        type: 'group',
+        children: [
+          { key: '3', label: 'Option 3' },
+          { key: '4', label: 'Option 4' },
+        ],
+      },
+    ],
+  },
+];
+const App = () => {
+  const onClick = e => {
+    console.log('click ', e);
+  };
   return (
-    <div>
-      <MyCard/>
-    </div>
-  )
-}
-
-export default App
+    <Menu
+      onClick={onClick}
+      style={{ width: 256 }}
+      defaultSelectedKeys={['1']}
+      defaultOpenKeys={['sub1']}
+      mode="inline"
+      items={items}
+    />
+  );
+};
+export default App;
